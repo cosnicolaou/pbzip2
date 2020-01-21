39fd95dd88ec14f07b60bb78a571b25509ea4cbf
Cosmos Nicolaou <cosmos.nicolaou@gmail.com>
2020-01-21 11:30:31 -0800

    add copyright notice

--- cmd/pbzip2/main.go
+++ cmd/pbzip2/main.go
@@ -1,3 +1,6 @@
+// Copyright 2020 Cosmos Nicolaou. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
 package main
 
 import (
